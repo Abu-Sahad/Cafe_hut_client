b5c8503a54ed883873f155c9f6076e21060cce02
Abu-Sahad <abusahad603@gmail.com>
2023-05-25 13:58:51 +0600

    banner added

--- src/pages/ChefBanner/ChefBanner.jsx
+++ src/pages/ChefBanner/ChefBanner.jsx
@@ -1,7 +1,51 @@
+import { Carousel } from "react-bootstrap";
+import banner_1 from "../../assets/bannerimage-1.jpg"
+import banner_2 from "../../assets/bannerimage-2.jpg"
+import banner_3 from "../../assets/bannerimage-3.jpg"
 const ChefBanner = () => {
     return (
-        <div>
-            <h1>chef banner</h1>
+        <div className="container mb-5">
+            <Carousel fade>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 rounded"
+                        src={banner_1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Handkerchief Bef Rice</h3>
+                        <p>A handful of simple ingredients make Rice, beef, and flour, vibrant cooking flavors delicious</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 rounded"
+                        src={banner_2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3 className="text-dark">Baked Chicken Legs</h3>
+                        <p className="text-dark">A handful of simple ingredients typify the Chicken, vibrant flavors of Greek cooking.
+
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 rounded"
+                        src={banner_3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3 className="text-dark">Chef Are Cooking prepare</h3>
+                        <p className="text-dark">
+                            Cooking is an art. The more time and effort you put into cooking it, the more fun and delicious it will be
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 };
